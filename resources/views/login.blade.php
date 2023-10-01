c6ff32c322bf290d764cf5ecaf3d6274fe36f6ee
Maksim Mikhailov <2618094@gmail.com>
2023-10-01 22:22:27 +0300

    fix login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -164,15 +164,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Local socialite</title>
+    <title>Socialite Local</title>
 </head>
 <body>
     <div id="app">
         <div class="login">
             <div class="login-wrapper">
                 <div class="login-column">
-                    <h2>Info</h2>
-                    <p class="login-info-item">Original Driver: <span>{{ $original_driver }}</span></p>
+                    <h2>Request</h2>
+                    <p class="login-info-item">Driver: <span>{{ $original_driver }}</span></p>
                     <p class="login-info-item">Client ID: <span>{{ $client_id }}</span></p>
                     <p class="login-info-item">Scopes: <span>{{ $scope }}</span></p>
                     <p class="login-info-item">State: <span>{{ $state ?? '' }}</span></p>
@@ -180,10 +180,10 @@
                     <p class="login-info-item">Redirect: <span>{{ $redirect_uri }}</span></p>
                 </div>
                 <div class="login-column center">
-                    <h2>Login</h2>
+                    <h2>Response</h2>
                     <form class="login-form" name="login-form" action="{{ \Illuminate\Support\Facades\URL::route('socialite_local.login') }}" method="post">
                         <span class="login-text">@csrf</span>
-                        <h3>Response</h3>
+                        <h3>Authorization</h3>
                         <div class="login-form-item">
                             <label for="scope" class="form-label">Scope</label>
                             <input type="text" class="form-control" id="scope" name="scope" value="{{ $scope }}">
@@ -209,7 +209,7 @@
                         </div>
                         <div class="login-actions">
                             <button type="submit" class="login-button">
-                                Submit
+                                Login
                             </button>
 {{--                            <button type="button" class="login-add-button" onclick="addCustomFields()">--}}
 {{--                                Add fields--}}
